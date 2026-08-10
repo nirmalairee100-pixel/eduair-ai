4b801cf8fc9e9c99726604ae19b4814677df105d
Nirmal Airee <nirmalairee100@gmail.com>
2026-08-10 18:17:56 +0545

    Add Google AdSense script to root layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import RegisterServiceWorker from "@/components/RegisterServiceWorker";
 
@@ -38,6 +39,12 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6534485204520302"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         {children}
         <RegisterServiceWorker />
       </body>
